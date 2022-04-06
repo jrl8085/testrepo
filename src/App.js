f0bcb09682f0862ead13fab7486b33f0747a6ad5
jrl8085 <jasonrloomis@gmail.com>
2022-04-06 14:39:06 -0500

    New Footer Branch creation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          woooooo
+          woooooot
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is my footer update
         </a>
       </header>
     </div>
