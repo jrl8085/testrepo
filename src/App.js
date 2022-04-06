149b61a80fbde22d24b5a60a0cd31adac3cc4418
jrl8085 <jasonrloomis@gmail.com>
2022-04-06 13:45:38 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New Title 2s<code>src/App.js</code> and save to reload.....
+          New Title 2s<code>src/App.js</code> and save to reload.....sdafsadf
         </p>
         <a
           className="App-link"
